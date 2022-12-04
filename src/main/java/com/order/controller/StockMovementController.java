fb1d5cb933279104a7a0bd52c25586d6b8e960cb
luisNovaes <luismagnovaes@gmail.com>
2022-12-04 22:45:48 +0000

    When an order is completed, send a notification by email to the user who created it

--- src/main/java/com/order/controller/StockMovementController.java
+++ src/main/java/com/order/controller/StockMovementController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.order.OrderManagerApplication;
 import com.order.model.Item;
 import com.order.model.StockMovement;
 import com.order.repository.ItemRepository;
